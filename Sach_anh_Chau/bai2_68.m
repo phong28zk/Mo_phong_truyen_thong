3a40f5d61c9583329e4fad4831587b529c29798b
Leon28Phongretzka <lamperougelelouchanm@gmail.com>
2023-03-03 14:33:12 +0700

    03 03 23

--- Sach_anh_Chau/bai2_68.m
+++ Sach_anh_Chau/bai2_68.m
@@ -4,6 +4,26 @@
 
 
 function [A] = bai2_68()
+    figure;
+    domain = -4:0.01:4;
+    plot(domain, normpdf(domain, 0, 1), 'r');
+    hold on;
+    plot(domain, normpdf(domain, 0, 2), 'b');
+    hold on;
+
+    L = fix(length(domain)/3);
+    R = fix(length(domain)*2/3);
+
+    area(domain(1:L), normpdf(domain(1:L), 0, 1), 'FaceColor', 'r');
+    area(domain(R:end), normpdf(domain(R:end), 0, 1), 'FaceColor', 'r');
+
+    stem([-0.7,0.1,0.4], normpdf([-0.7,0.1,0.4], 0, 1), 'r', 'filled');
     
+    axis([-4 4 0 0.5]);
+    
+    set(gca, 'XTick', on);
+    set(gca, 'YTick', on);
+
+
 end
 
